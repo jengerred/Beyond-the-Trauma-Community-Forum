3568980195fafbce536d54b655f4081ca0b1664c
jengerred <56004257+jengerred@users.noreply.github.com>
2023-10-20 20:14:53 -0400

    added paragraph

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,8 @@ import ResourcesPage from './ResourcesPage';
 function HomePage() {
   return (
     <>
-    <h3 style={{margin: "1rem", textAlign: "center"}}>Beyond The Trauma Community Forum</h3>
-    <h5 style={{margin: "1rem", textAlign: "center"}}> A platform for women who are domestic violence survivors to find peer support, information, and resources.</h5>
+    <h1 style={{margin: "1rem", textAlign: "center"}}>Beyond The Trauma Community Forum</h1>
+    <h4 style={{margin: "1rem", textAlign: "center"}}> A platform for women who are domestic violence survivors to find peer support, information, and resources.</h4>
     <div style={{height: "10rem"}}>
       <p style={{margin: "1rem"}}>
         Description of what the project is and why 

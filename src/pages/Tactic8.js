e57bdafc90f8e82cb638b2d3f81deb11e19dd781
Kara Evans <118031032+ke177409@users.noreply.github.com>
2023-10-20 22:00:52 -0400

    Update Tactic8.js
    modifying title and list

--- src/pages/Tactic8.js
+++ src/pages/Tactic8.js
@@ -5,10 +5,10 @@ function Tactic8() {
   return (
 <>
 <ForumData
-   title="Using Economic Abuse"
+   title="Economic Abuse"
    text="Some examples of using economic abuse are:" 
    list ="preventing her from getting or keeping a job"
-   list2="mmaking her ask for money"
+   list2="making her ask for money"
    list3="giving her an allowance"
    list4 = "taking her money"
    list5 = "not letting her know about or have access to family income"

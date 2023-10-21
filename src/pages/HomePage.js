41c3c9c94eb62a7cb5f461464bd4d45c4067eb89
Kara Evans <118031032+ke177409@users.noreply.github.com>
2023-10-20 21:39:24 -0400

    Update HomePage.js
    adding description of project and why it's important

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
     <h4 style={{margin: "1rem", textAlign: "center"}}> A platform for women who are domestic violence survivors to find peer support, information, and resources.</h4>
     <div style={{height: "10rem"}}>
       <p style={{margin: "1rem"}}>
-        Description of what the project is and why 
+        Domestic violence is a complex issue that affects millions of women worldwide. Many women ask for help but cannot get the resources they need due to social barriers or limited resources. This disucssion forum will provide a safe environment for users to share stories, access resources, support each other, and, most of all, begin to <b>heal</b>.
       </p>
     </div>
 

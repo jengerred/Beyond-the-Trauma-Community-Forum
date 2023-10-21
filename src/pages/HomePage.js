47ff30fd393219685f33a4305c0fbe0dadef5b41
Kara Evans <118031032+ke177409@users.noreply.github.com>
2023-10-21 15:28:30 -0400

    Update HomePage.js
    undoing previous changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import TopicsPage from './TopicsPage';
 import ResourcesPage from './ResourcesPage';
-import hero_image from '/images/hero_image.jpg';
 
 function HomePage() {
   return (
     <>
-<column style={{   
-  height: "fit-content",
-   margin: "auto",
-   textAlign: "center"}}>
- <ol style={{listStyle: "none"}}>
-  
     <h1 style={{margin: "1rem", textAlign: "center"}}>Beyond The Trauma Community Forum: Grand Rapids, Michigan</h1>
     <h4 style={{margin: "1rem", textAlign: "center"}}> A platform for women in the Grand Rapids area who are domestic violence survivors to find peer support, information, and resources.</h4>
     <div style={{height: "10rem"}}>
@@ -20,45 +13,6 @@ function HomePage() {
         Your safety is our top priority, which is why our registration process is <b>secure</b> and <b>anonymous</b>. We have implemented a passwordless system using passkeys that require a fingerprint, face scan, or a personal identification number (PIN). To get started, create a username and setup a passkey.
       </p>
     </div>
-  
-</ol>
-</column>
-
-        <column>
-      
-        <img src={hero_image} className="image" />
-
-        </column>
-
-        <style jsx>
-        {`
-        .heading-text {
-          text-align: left;
-        }
-    
-        .image {
-            border: 2px solid black;
-            margin-left: 50%;
-            margin-top: -50rem;
-            margin-bottom: 10rem;
-            width: 45%;
-            min-width: 18rem;
-          
-        }
-        @media screen and (max-width: 1200px) {
-          .image {
-            display: grid;
-            margin: auto;
-          }
-        }
-       
-        `}
-        </style>
-
-</div>
-
-
- </>
 
     <TopicsPage/>
     <ResourcesPage/>

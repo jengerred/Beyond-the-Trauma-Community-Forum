ea2dfc658c6a156b242d847183e2a93ca1bfd527
Kara Evans <118031032+ke177409@users.noreply.github.com>
2023-10-20 22:00:14 -0400

    Update Tactic7.js
    modifying title and list

--- src/pages/Tactic7.js
+++ src/pages/Tactic7.js
@@ -5,9 +5,9 @@ function Tactic7() {
   return (
     <>
        <ForumData
-   title="Using Male Privilege"
+   title="Male Privilege"
    text="Some examples of using male privilege are:" 
-   list ="ptreating her like a servant"
+   list ="treating her like a servant"
    list2="making all the big decisions"
    list3="acting like the “master of the castle”"
    list4 = "being the one to define men’s and women’s roles"

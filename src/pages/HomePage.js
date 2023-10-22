d73561fffa99fa8d0820a4087f29d83e9e2dd346
jengerred <56004257+jengerred@users.noreply.github.com>
2023-10-22 11:36:53 -0400

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,24 +6,30 @@ import HeroImage from '../images/hero_image.jpg';
 function HomePage() {
   return (
     <>
+
     <h1 style={{margin: "1rem", textAlign: "center"}}>Beyond The Trauma Community Forum: Grand Rapids, Michigan</h1>
     <h4 style={{margin: "1rem", textAlign: "center"}}> A platform for women in the Grand Rapids area who are domestic violence survivors to find peer support, information, and resources.</h4>
     
-    <div className="home-container" style={{height: "20rem"}}>
-    <div className="home-image">
-      <img src={HeroImage} className="hero-image" />
-      </div>
+    <div className="home-container" >
+
+        <div className="home-image">
+          <img src={HeroImage} className="hero-image" />
+        </div>
+
       <p style={{margin: "1rem"}}>
         Domestic violence is a complex issue that affects millions of women worldwide. Many women ask for help but cannot get the resources they need due to social barriers or limited resources. This disucssion forum will provide a safe environment for users to share stories, access resources, support each other, and, most of all, begin to <b>heal</b>.
         Your safety is our top priority, which is why our registration process is <b>secure</b> and <b>anonymous</b>. We have implemented a passwordless system using passkeys that require a fingerprint, face scan, or a personal identification number (PIN). To get started, create a username and setup a passkey.
       </p>
-      
+    
      
       <style jsx>
       {`
       .home-container{
         display: grid;
         grid-template-columns: min-content 1fr;
+        height: fit-content;
+        margin: 1rem;
+        margin-bottom: 2rem;
       }
 
       .hero-image{
@@ -41,6 +47,7 @@ function HomePage() {
 
       </style>
     </div>
+
 
     <TopicsPage/>
     <ResourcesPage/>
